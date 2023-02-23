721b273522f8b050b61b500f75f7a20d6183b910
Nrochester <123398837+Nrochester@users.noreply.github.com>
2023-02-23 10:00:00 -0500

    airport exercise complete

--- Bag.js
+++ Bag.js
@@ -1,8 +1,19 @@
 class Bag {
+    constructor(weight) {
+        if(!weight) {throw new Error('bag needs weight');
+    }
+    this.weight = weight
+    }
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
   
 }
+try {
+    const bag = new Bag();
+} catch(err) {
+    console.log(err)
+}
 
 //Export the module
+module.exports = Bag
--- Person.js
+++ Person.js
@@ -1,10 +1,20 @@
 class Person {
+    constructor(name,bags) {
+        this.name = name
+        this.bags = []
+    }
     // Include a Constructor Method for Properties: 
         // 1. Name
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
+    addBag(bag) {
+    this.bags.push(bag)
+}
+
 }
 
 //Export the Module
+
+module.exports = Person
